1150e9944721b3a2c41c483487bccc935de80f60
MonicaItzel <taverahernandezmonicaitzel@gmail.com>
2024-07-12 09:49:59 -0600

    Creación del servicio y datos
    Creación del servicio y datos estáticos

---
+++ cart-app/src/app/data/product-data.ts
@@ -0,0 +1,52 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: 'Ramo de novia',
+        description: 'Ramo personalizado para novia',
+        price: 800,
+    },
+    {
+        id: 2,
+        name: 'Ramo de novia',
+        description: 'Ramo personalizado para novia',
+        price: 1000,
+    },
+    {
+        id: 3,
+        name: 'Ramo de novia',
+        description: 'Ramo personalizado para novia',
+        price: 1200,
+    },
+    {
+        id: 4,
+        name: 'Ramo de amigas',
+        description: 'Ramo personalizado para novia',
+        price: 1000,
+    },
+    {
+        id: 5,
+        name: 'Ramo de mama',
+        description: 'Ramo personalizado para novia',
+        price: 500,
+    },
+    {
+        id: 6,
+        name: 'Ramo papá',
+        description: 'Ramo personalizado para novia',
+        price: 650,
+    },
+    {
+        id: 7,
+        name: 'Ramo violeta',
+        description: 'Ramo personalizado para regalar a esa persona alguién especial',
+        price: 550,
+    },
+    {
+        id: 8,
+        name: 'Ramo coral',
+        description: 'Ramo personalizado para regalar a esa persona alguién especial',
+        price: 760,
+    },
+]
